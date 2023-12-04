345b3a493b00342277c5829cc3764bdf5ce4d559
sriya <spingili@albany.edu>
2023-12-04 16:06:35 -0500

    adding python files

---
+++ Python_Level1/Q1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a = int(input())
+if a%3==0 and a%5==0:
+    print("Consultadd-Python Training")
+elif a%5==0:
+    print("Python Training")
+elif a%3==0 :
+    print("Consultadd")
+
+
+
+
+
+
